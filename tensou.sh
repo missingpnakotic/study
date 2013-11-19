d1c12fd3f7389a95fd6dc0dbc420d8ae752fa0dc
ｄふぁｄ <>
2013-11-19 18:18:34 +0900

    世界用WWW
    英語にしたら、俺があんま概要理解できないｗｗ

--- tensou.sh
+++ tensou.sh
@@ -1,5 +1,54 @@
-#!/bin/sh -x
-
+#!/bin/sh
+#####################################################################################################
+# Outline of processing                                                                             #
+# Asset name  tensou.sh                                                                             #
+# Processing content                                                                                #
+# Get file date list day of mission-critical systems has created.                                   #
+# I determine retrieve target data listed in the list file date day.                                #
+#                                                                                                   #
+# Notices                                                                                           #
+# Nothing in particular                                                                             #
+#                                                                                                   #
+#                                                                                                   #
+# Input directory name :${home}${inlist}                                                            #
+# Input directory name :${home}${indata}                                                            #
+# Target file name     :E${kaisya}${daylist}hhmmdd                                                  #
+# Output directory name:${home}${outdata}	                                                    #
+# Target list file     :E${kaisya}${daylist}hhmmdd.list                                             #
+#                                                                                                   #
+#                                                                                                   #
+# Processing details                                                                                #
+# 1.Environment variable settings                                                                   #
+#                                                                                                   #
+# 2.Parameter check                                                                                 #
+# 	If abnormal, the process is completed and outputs a log.                                    #
+#                                                                                                   #
+# 3.Check presence of the target directory                                                          #
+# 	If the directory does not exist, and the process ends and outputs the log.		    #
+#                                                                                                   #
+# 4.Get a list file                                                                                 #
+# I get a target list of file date day.                                                             #
+# 	If the target list file does not exist,                                                     #
+#         I make conditional normal termination and the process output to the log to that effect.   #
+#                                                                                                   #
+# 5.Acquisition of the target file                                                                  #
+# From the description date information list file, I get a target file that day.                    #
+# 	If the target file is not listed, thereby conditional normal termination                    #
+#         processing is output to the log to that effect.	                                    #
+#                                                                                                   #
+# 	If the target file that is listed does not exist,                                           #
+#         I make conditional normal termination and the process output to the log to that effect.   #
+#                                                                                                   #
+# 6.Target file check                                                                               #
+# 	If abnormal, the process is completed and outputs a log.                                    #
+#                                                                                                   #
+#                                                                                                   #
+#                                                                                                   #
+#                                                                                                   #
+#                                                                                                   #
+#                                                                                                   #
+#                                                                                                   #
+#####################################################################################################
 ### #3=question
 ### #!/usr/bin/env bash or #!/bin/sh
 ### I want to using JAPANESE in github
